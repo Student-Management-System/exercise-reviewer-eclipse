a334141cdcf71584a8c5f4582ea0d4f85c16a9be
Lukas Dönges <lukasdoenges@t-online.de>
2021-10-21 19:51:54 +0200

    add refreshbutton to taskbar

--- src/net/ssehub/teaching/exercise_reviewer/eclipse/views/AllReviewableSubmissionsView.java
+++ src/net/ssehub/teaching/exercise_reviewer/eclipse/views/AllReviewableSubmissionsView.java
@@ -6,8 +6,10 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
 import org.eclipse.ui.part.ViewPart;
 import org.osgi.framework.Bundle;
 
@@ -21,6 +23,8 @@ import net.ssehub.teaching.exercise_reviewer.eclipse.Activator;
 public class AllReviewableSubmissionsView extends ViewPart {
     private Label label;
     private Action getAllSubmissionAction;
+    private Action downloadAllSubmissionAction;
+    private List swtList;
 
     /**
      * Creates an instance of the ReviewView class.
@@ -47,7 +51,7 @@ public class AllReviewableSubmissionsView extends ViewPart {
      * Creates the action.
      */
     public void createActions() {
-        getAllSubmissionAction = new Action("Add...") {
+        getAllSubmissionAction = new Action("Get list of submissions") {
             /**
              * Creates the button press action.
              */
@@ -55,7 +59,19 @@ public class AllReviewableSubmissionsView extends ViewPart {
                 System.out.println("");
             }
         };
-        getAllSubmissionAction.setImageDescriptor(getImageDescriptor("icons/download.png"));
+        getAllSubmissionAction.setImageDescriptor(getImageDescriptor("icons/refresh.png"));
+        
+        downloadAllSubmissionAction = new Action("Download all submissions") {
+            /**
+             * Creates the button press action.
+             */
+            public void run() {
+                System.out.println("");
+            }
+        };
+        downloadAllSubmissionAction.setImageDescriptor(getImageDescriptor("icons/download.png"));
+        
+        
 
     }
     /**
@@ -64,8 +80,18 @@ public class AllReviewableSubmissionsView extends ViewPart {
     private void createToolbar() {
 
         IToolBarManager mgr = getViewSite().getActionBars().getToolBarManager();
+        mgr.add(downloadAllSubmissionAction);
         mgr.add(getAllSubmissionAction);
 
+    }
+    
+    private void createList(Composite parent) {
+        this.swtList = new List(parent, SWT.SINGLE);
+        
+     //   this.swtList.add();
+        
+        
+        
     }
     /**
      * Gets the imagedescriptor from the relative icon path.
