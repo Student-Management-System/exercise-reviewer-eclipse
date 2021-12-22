799e591172a5ebc35e705e9ee5226e4fcaa46d5b
Lukas Dönges <lukasdoenges@t-online.de>
2021-12-22 20:46:16 +0100

    refactoring download all submissionsjob

--- src/net/ssehub/teaching/exercise_reviewer/eclipse/background/DownloadAllSubmissionsJob.java
+++ src/net/ssehub/teaching/exercise_reviewer/eclipse/background/DownloadAllSubmissionsJob.java
@@ -1,26 +1,12 @@
 package net.ssehub.teaching.exercise_reviewer.eclipse.background;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UncheckedIOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -34,10 +20,9 @@ import net.ssehub.teaching.exercise_reviewer.eclipse.Activator;
 import net.ssehub.teaching.exercise_reviewer.eclipse.dialog.ExceptionDialog;
 import net.ssehub.teaching.exercise_reviewer.eclipse.dialog.DownloadAllResultDialog;
 import net.ssehub.teaching.exercise_reviewer.eclipse.log.EclipseLog;
+import net.ssehub.teaching.exercise_reviewer.eclipse.submissions.DownloadSubmission;
 import net.ssehub.teaching.exercise_submitter.lib.ExerciseSubmitterManager;
 import net.ssehub.teaching.exercise_submitter.lib.data.Assignment;
-import net.ssehub.teaching.exercise_submitter.lib.replay.ReplayException;
-import net.ssehub.teaching.exercise_submitter.lib.replay.Replayer;
 import net.ssehub.teaching.exercise_submitter.lib.student_management_system.ApiException;
 import net.ssehub.teaching.exercise_submitter.lib.student_management_system.CourseNotSelectedException;
 import net.ssehub.teaching.exercise_submitter.lib.student_management_system.IApiConnection;
@@ -154,17 +139,12 @@ public class DownloadAllSubmissionsJob extends ReviewerJobs {
             List<String> allGroups = new ArrayList<>(api.getAllGroups(manager.getCourse(), this.assignment));
 
             SubMonitor submonitor = SubMonitor.convert(monitor, allGroups.size());
-            List<Replayer> replayers = new LinkedList<>();
-
-            replayProjects(manager, allGroups, submonitor, replayers);
+        
+            replayProjects(manager, allGroups, submonitor);
 
             this.createWorkingSetAndAddProjects(workingsetmanager);
 
-            copyDownloadedProjects();
-                     
-            
-            closeReplayer(replayers);
-            
+          
             Display.getDefault().syncExec(() -> {
                 DownloadAllResultDialog dialog = new DownloadAllResultDialog(getShell().orElse(new Shell()), projects);
                 dialog.open();
@@ -183,77 +163,23 @@ public class DownloadAllSubmissionsJob extends ReviewerJobs {
      * @param manager
      * @param allGroups
      * @param submonitor
-     * @param replayers
      * @throws CourseNotSelectedException
      */
-    private void replayProjects(ExerciseSubmitterManager manager, List<String> allGroups, SubMonitor submonitor,
-            List<Replayer> replayers) throws CourseNotSelectedException {
+    private void replayProjects(ExerciseSubmitterManager manager,
+            List<String> allGroups, SubMonitor submonitor) throws CourseNotSelectedException {
+        
         for (String group : allGroups) {
             Project project = new Project(group);
-            try {
-                Replayer replayer = manager.getReplayer(this.assignment, group);
-                replayers.add(replayer);
-                
-                File temporaryCheckout = replayer.replayLatest();
-                submonitor.split(1).done();
-                project.setFile(temporaryCheckout);
-                createIProject(project);
-            } catch (ReplayException | CoreException e) {
-                project.setException(e);
-            }
+       
+            DownloadSubmission submissions = new DownloadSubmission(group, project,
+                   assignment, manager);
+            submissions.start();
+       
             this.projects.add(project);
             submonitor.split(1).done();
         }
     }
-    /**
-     * Close all replayer -> Deletes all temp folders.
-     * @param replayers
-     */
-    private void closeReplayer(List<Replayer> replayers) {
-        for (Replayer replayer : replayers) {
-            try {
-                replayer.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-    }
-    /**
-     * Creates the IProject.
-     *
-     * @param project
-     * @return boolean , true if it worked.
-     * @throws CoreException
-     */
-    private boolean createIProject(Project project) throws CoreException {
-        boolean isCreated = false;
-    
-        String groupname = project.getGroupName();
-        String projectName = "Submission from " + groupname + " for " + assignment.getName();
-        IWorkspace workspace = ResourcesPlugin.getWorkspace();
-        IWorkspaceRoot root = workspace.getRoot();
-        IProject newProject = root.getProject(projectName);
-        newProject.create(null);
-        newProject.open(null);
-        project.setIProject(newProject);
-        isCreated = true;
-    
-    
-        try {
-            IProjectDescription description = newProject.getDescription();
-            description.setNatureIds(new String[] {"org.eclipse.jdt.core.javanature"});
-            newProject.setDescription(description, null);
-        } catch (CoreException e) {
-            EclipseLog.warning("Failed to set java nature for new project: " + e.getMessage());
-        }
-    
-        Activator.getDefault().getProjectManager()
-        .setConnection(project.getProject().get(), groupname, assignment.getManagementId());
-    
-        return isCreated;
-    }
-
+   
     /**
      * Creates the workingset and adds the projects.
      * @param workingsetmanager
@@ -283,69 +209,24 @@ public class DownloadAllSubmissionsJob extends ReviewerJobs {
             workingsetmanager.addWorkingSet(newSet);
             //TODO: need to refresh gui or something
         }
+        
     }
-
-    /**
-     * Copys the downloaded files to the projectfolders.
-     */
-    private void copyDownloadedProjects() {
-        for (Project element: this.projects) {
-            if (element.isSucceeded()) {
-                try {
-                    this.copyProject(element.getFile().get().toPath(), 
-                            element.project.get().getLocation().toFile().toPath());
-                    this.copyDefaultClasspath(element.project.get().getLocation().toFile().toPath());
-                    element.project.get().refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
-                } catch (IOException | CoreException e) {
-                    //element.setException(e);
-                }
-            }
-        }
-    }
-    /**
-     * Copies the project from the temp folder to the project folder.
-     *
-     * @param source
-     * @param target
-     * @throws IOException
-     */
-    private void copyProject(Path source, Path target) throws IOException {
-        try {
-            Files.walk(source).forEach(sourceFile -> {
-                Path targetFile = target.resolve(source.relativize(sourceFile));
     
-                try {
-                    if (Files.isDirectory(sourceFile)) {
-                        if (!Files.exists(targetFile)) {
-                            Files.createDirectory(targetFile);
-                        }
-                    } else {
-                        Files.copy(sourceFile, targetFile, StandardCopyOption.REPLACE_EXISTING);
-                    }
-                } catch (IOException e) {
-                    throw new UncheckedIOException(e);
-                }
-            });
-        } catch (UncheckedIOException e) {
-            throw e.getCause();
-        }
-    }
+//    private WorkingSetModel getWorkingSetModel() {
+//        WorkingSetModel result = null;
+//        P
+//        IWorkbenchPage page = JavaPlugin.getActivePage();
+//
+//        if (page != null) {
+//            IWorkbenchPart activePart = page.getActivePart();
+//
+//            if (activePart instanceof PackageExplorerPart) {
+//                result = ((PackageExplorerPart) activePart).getWorkingSetModel();
+//            }
+//        }
+//
+//        return result;
+//    }
 
-    /**
-     * Copies the default <code>.classpath</code> file from the resources to the given target directory.
-     *
-     * @param targetDirectory The directory where to create the <code>.classpath</code> file in.
-     *
-     * @throws IOException If creating the file fails.
-     */
-    private void copyDefaultClasspath(Path targetDirectory) throws IOException {
-        InputStream in = this.getClass().getResourceAsStream(".classpath");
-        if (in != null) {
-            Files.copy(in, targetDirectory.resolve(".classpath"));
-        } else {
-            throw new IOException(".classpath resource not found");
-        }
-    }
-
-
+   
 }
---
+++ src/net/ssehub/teaching/exercise_reviewer/eclipse/submissions/DownloadSubmission.java
@@ -0,0 +1,173 @@
+package net.ssehub.teaching.exercise_reviewer.eclipse.submissions;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+
+import net.ssehub.teaching.exercise_reviewer.eclipse.Activator;
+import net.ssehub.teaching.exercise_reviewer.eclipse.background.DownloadAllSubmissionsJob.Project;
+import net.ssehub.teaching.exercise_reviewer.eclipse.log.EclipseLog;
+import net.ssehub.teaching.exercise_submitter.lib.ExerciseSubmitterManager;
+import net.ssehub.teaching.exercise_submitter.lib.data.Assignment;
+import net.ssehub.teaching.exercise_submitter.lib.replay.ReplayException;
+import net.ssehub.teaching.exercise_submitter.lib.replay.Replayer;
+import net.ssehub.teaching.exercise_submitter.lib.student_management_system.CourseNotSelectedException;
+
+/**
+ * Handles the download from a user submission.
+ * 
+ * @author lukas
+ *
+ */
+public class DownloadSubmission {
+    
+    private String groupname;
+    private Project project;
+    private Assignment assignment;
+    private ExerciseSubmitterManager manager;
+    
+    /**
+     * Creates instance of {@link #DownloadSubmission}.
+     * @param groupname
+     * @param project
+     * @param assignment
+     * @param manager
+     */
+    public DownloadSubmission(String groupname, Project project,
+            Assignment assignment, ExerciseSubmitterManager manager) {
+        this.groupname = groupname;
+        this.project = project;
+        this.assignment = assignment;
+        this.manager = manager;
+    }
+    /**
+     * Start the download.
+     * 
+     * @return Project
+     * @throws CourseNotSelectedException
+     */
+    public Project start() throws CourseNotSelectedException {
+        replay();       
+        return project;    
+    }
+    /**
+     * Replay the submission from the group in the course.
+     * 
+     * @throws CourseNotSelectedException
+     */
+    private void replay() throws CourseNotSelectedException {
+        try {
+            Replayer replayer = manager.getReplayer(this.assignment, groupname);
+                   
+            File temporaryCheckout = replayer.replayLatest();
+        
+            project.setFile(temporaryCheckout);
+            
+            createIProject();
+            
+            copyProject(project.getFile().get().toPath(), project.getProject().get().getLocation().toFile().toPath());
+            copyDefaultClasspath(project.getProject().get().getLocation().toFile().toPath());
+            project.getProject().get().refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+            replayer.close();
+            
+        } catch (ReplayException | CoreException | IOException e) {
+            project.setException(e);
+        }
+    }
+    /**
+     *  Creates for the downloaded submission a IProject in
+     *  Eclipse.
+     *        
+     *        
+     * @return boolean
+     * @throws CoreException
+     */
+    private boolean createIProject() throws CoreException {
+        boolean isCreated = false;
+    
+        String groupname = project.getGroupName();
+        String projectName = "Submission from " + groupname + " for " + assignment.getName();
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
+        IWorkspaceRoot root = workspace.getRoot();
+        IProject newProject = root.getProject(projectName);
+        newProject.create(null);
+        newProject.open(null);
+        project.setIProject(newProject);
+        isCreated = true;
+    
+    
+        try {
+            IProjectDescription description = newProject.getDescription();
+            description.setNatureIds(new String[] {"org.eclipse.jdt.core.javanature"});
+            newProject.setDescription(description, null);
+        } catch (CoreException e) {
+            EclipseLog.warning("Failed to set java nature for new project: " + e.getMessage());
+        }
+    
+        Activator.getDefault().getProjectManager()
+        .setConnection(project.getProject().get(), groupname, assignment.getManagementId());
+    
+        return isCreated;
+    }
+
+   
+    /**
+     * Copies the project from the temp folder to the project folder.
+     *
+     * @param source
+     * @param target
+     * @throws IOException
+     */
+    private void copyProject(Path source, Path target) throws IOException {
+        try {
+            Files.walk(source).forEach(sourceFile -> {
+                Path targetFile = target.resolve(source.relativize(sourceFile));
+    
+                try {
+                    if (Files.isDirectory(sourceFile)) {
+                        if (!Files.exists(targetFile)) {
+                            Files.createDirectory(targetFile);
+                        }
+                    } else {
+                        Files.copy(sourceFile, targetFile, StandardCopyOption.REPLACE_EXISTING);
+                    }
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            });
+        } catch (UncheckedIOException e) {
+            throw e.getCause();
+        }
+    }
+
+    /**
+     * Copies the default <code>.classpath</code> file from the resources to the given target directory.
+     *
+     * @param targetDirectory The directory where to create the <code>.classpath</code> file in.
+     *
+     * @throws IOException If creating the file fails.
+     */
+    private void copyDefaultClasspath(Path targetDirectory) throws IOException {
+        InputStream in = this.getClass().getResourceAsStream(".classpath");
+        if (in != null) {
+            Files.copy(in, targetDirectory.resolve(".classpath"));
+        } else {
+            throw new IOException(".classpath resource not found");
+        }
+    }
+    
+
+}
